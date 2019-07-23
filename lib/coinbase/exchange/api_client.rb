bb945ac2b02a940b56bdb6738068e873e1754452
wmuenigneer <adam.d.bell@icloud.io>
2019-07-23 10:55:46 -0700

    break more and more tests

--- lib/coinbase/exchange/api_client.rb
+++ lib/coinbase/exchange/api_client.rb
@@ -91,17 +91,17 @@ module Coinbase
         out
       end
 
-      # def daily_stats(params = {})
-      #   product = params[:product_id] || @default_product
+      def daily_stats(params = {})
+        product = params[:product_id] || @default_product
 
-      #   out = nil
-      #   get("/products/#{product}/stats", params) do |resp|
-      #     resp["start"] = (Time.now - 24 * 60 * 60).to_s
-      #     out = response_object(resp)
-      #     yield(out, resp) if block_given?
-      #   end
-      #   out
-      # end
+        out = nil
+        get("/products/#{product}/stats", params) do |resp|
+          resp["start"] = (Time.now - 24 * 60 * 60).to_s
+          out = response_object(resp)
+          yield(out, resp) if block_given?
+        end
+        out
+      end
 
       #
       # Accounts
@@ -124,14 +124,14 @@ module Coinbase
         out
       end
 
-      def account_history(id, params = {})
-        out = nil
-        get("/accounts/#{id}/ledger", params, paginate: true) do |resp|
-          out = response_collection(resp)
-          yield(out, resp) if block_given?
-        end
-        out
-      end
+      # def account_history(id, params = {})
+      #   out = nil
+      #   get("/accounts/#{id}/ledger", params, paginate: true) do |resp|
+      #     out = response_collection(resp)
+      #     yield(out, resp) if block_given?
+      #   end
+      #   out
+      # end
 
       def account_holds(id, params = {})
         out = nil
